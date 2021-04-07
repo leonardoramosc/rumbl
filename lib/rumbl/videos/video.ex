6877b92285b5bf993c1fd5108a1d2c8bdb27bee1
leonardoramosc <leorrc96@gmail.com>
2021-04-07 17:15:45 -0400

    Ahora antes de crear un video se crea un slug que sera colocado posteriormente en el url

--- lib/rumbl/videos/video.ex
+++ lib/rumbl/videos/video.ex
@@ -6,7 +6,9 @@ defmodule Rumbl.Videos.Video do
     field :description, :string
     field :title, :string
     field :url, :string
+    field :slug, :string
     field :user_id, :id
+
     belongs_to :category, Rumbl.Videos.Category
 
     timestamps()
@@ -17,6 +19,21 @@ defmodule Rumbl.Videos.Video do
     video
     |> cast(attrs, [:url, :title, :description, :category_id])
     |> validate_required([:url, :title, :description])
+    |> slugify_title()
     |> assoc_constraint(:category)
+  end
+
+  def slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
   end
 end
