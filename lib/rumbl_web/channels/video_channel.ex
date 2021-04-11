51162b9ce12fe22dceef6952b503fe92b3366cdf
leonardoramosc <leorrc96@gmail.com>
2021-04-11 12:28:12 -0400

    ahora desde el servidor se recibe el evento new_annotation y se envia la nueva anotattion al frontend para que todos puedan verlas

--- lib/rumbl_web/channels/video_channel.ex
+++ lib/rumbl_web/channels/video_channel.ex
@@ -2,6 +2,25 @@ defmodule RumblWeb.VideoChannel do
   use RumblWeb, :channel
 
   def join("videos:" <> video_id, _params, socket) do
-    {:ok, assign(socket, :video_id, String.to_integer(video_id))}
+    {:ok, socket}
   end
+
+  # esta funcion maneja los mensajes entrantes a este canal.
+  def handle_in("new_annotation", params, socket) do
+    # broadcast envia un evento a todos los usuarios en el topico actual
+    broadcast! socket, "new_annotation", %{
+      user: %{username: "anon"},
+      body: params["body"],
+      at: params["at"]
+    }
+
+    {:reply, :ok, socket}
+  end
+
+  # def handle_info(:ping, socket) do
+  #   count = socket.assigns[:count] || 0
+  #   push socket, "ping", %{count: count}
+
+  #   {:noreply, assign(socket, :count, count + 1)}
+  # end
 end
